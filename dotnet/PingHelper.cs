bd35fe20e10bc15b31f4447601905d28bef2ab9c
Tom Durrant <tomchdurrant@gmail.com>
2020-07-21 20:17:02 +0100

    Docker attempt 1

--- dotnet/PingHelper.cs
+++ dotnet/PingHelper.cs
@@ -1,69 +1,90 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Net.NetworkInformation;
-using System.Threading;
 using System.Timers;
 
 namespace Internet_Status
 {
-    internal static class PingHelper
+    internal class PingHelper
     {
-        private static string InternetStatsFile = "internet-stats.txt";
-
+        private const string InternetStatsFile = "internet-stats.txt";
         public static DateTime StartedPing { get; private set; }
         public static DateTime CompletedPing { get; private set; }
-        public static DateTime EventRaised { get; set; }
+        public static List<string> ListOfStatus { get; set; }
+        public static List<bool> ListOfInternetUp { get; set; }
+        public PingHelper()
+        {
+            ListOfStatus = new List<string>();
+            ListOfInternetUp = new List<bool>();
+        }
 
         internal static void DoThePing(object state, ElapsedEventArgs e)
         {
-            Ping pingSender = new Ping();
+            var pingSender = new Ping();
 
-            pingSender.PingCompleted += new PingCompletedEventHandler(PingCompletedCallback);
-
-            if (StartedPing == null)
-            {
-                StartedPing = new DateTime();
-            }
+            pingSender.PingCompleted += PingCompletedCallback;
 
             StartedPing = DateTime.Now;
 
-            pingSender.SendAsync("8.8.8.8", 120);
-
-            GC.Collect();
+            pingSender.SendAsync("8.8.8.8", 120, null);
         }
 
         private static void PingCompletedCallback(object sender, PingCompletedEventArgs e)
         {
-            if (CompletedPing == null)
-            {
-                CompletedPing = new DateTime();
-            }
-
             CompletedPing = DateTime.Now;
 
             var timeTaken = CompletedPing - StartedPing;
 
             if (e.Cancelled)
             {
-                System.Console.WriteLine("Ping cancelled");
+                Console.WriteLine("Ping cancelled");
             }
             else if (e.Error != null)
             {
-                System.Console.WriteLine($"It failed at {CompletedPing}");
-                using (StreamWriter file = new StreamWriter(InternetStatsFile, true))
+                Console.WriteLine($"It failed at {CompletedPing}");
+                using var file = new StreamWriter(InternetStatsFile, true);
+                var content = $"{DateTime.Now} - Internet down";
+                ListOfStatus.Add(content); 
+                ListOfInternetUp.Add(false);
+                file.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine($"It worked at {CompletedPing} in {timeTaken}ms");
+                using var file = new StreamWriter(InternetStatsFile, true);
+                var content = $"{DateTime.Now} - Internet up";
+                ListOfStatus.Add(content);
+                ListOfInternetUp.Add(true);
+                file.WriteLine(content);
+            }
+        }
+
+        public static void PrintToTxt(object sender, ElapsedEventArgs e)
+        {
+            var today = $"{DateTime.Now:yyyy-MM-dd}";
+
+            Console.WriteLine($"Printing stats for {today} at {DateTime.Now.TimeOfDay}");
+
+            if (ListOfInternetUp.Contains(false))
+            {
+                using var file = new StreamWriter($"{today}.txt", true);
+                foreach (var status in ListOfStatus)
                 {
-                    file.WriteLine($"{DateTime.Now} - Internet down");
+                    file.WriteLine(status);
                 }
             }
             else
             {
-                System.Console.WriteLine($"It worked at {CompletedPing} in {timeTaken}ms");
-                using (StreamWriter file = new StreamWriter(InternetStatsFile, true))
-                {
-                    file.WriteLine($"{DateTime.Now} - Internet up");
-                }
-
+                using var file = new StreamWriter($"{today}.txt", true);
+                file.WriteLine($"Internet up from {DateTime.Now - TimeSpan.FromHours(1)} until {DateTime.Now}");
             }
+            
+            
+                
+            ListOfStatus.Clear();
+            ListOfInternetUp.Clear();
         }
     }
 }
--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,44 +1,35 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Timers;
 
 namespace Internet_Status
 {
     class Program
     {
-        private const string InternetStatsFile = "internet-stats.txt";
-        private const int Period = 120_000;
+        private const int TwoMinutes = 200; // 2 minutes
+        private const int OneHour = 3_600_000; // 1 hour
 
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Ping google to test internet");
-            if (!File.Exists(InternetStatsFile))
-            {
-                FileStream fileStream = File.Create(InternetStatsFile);
-            }
-
-            Timer aTimer = new Timer(Period);
-            aTimer.Elapsed += PingHelper.DoThePing;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
+            Console.WriteLine("Ping google to test internet");
+            var pingHelper = new PingHelper();
+            var pingTimer = SetUpTimer(PingHelper.DoThePing, 2000);
+            var summaryTimer = SetUpTimer(PingHelper.PrintToTxt, 20000);
 
             Console.ReadLine();
         }
 
-        private static void WriteOutput(bool internetStatus, PingCompletedEventArgs e)
+        private static Timer SetUpTimer(ElapsedEventHandler onElapsed, double time)
         {
-            if (internetStatus)
-            {
-                System.Console.WriteLine("It worked");
-                System.Console.WriteLine($"Address: {e.Reply.Address.ToString()}");
-                System.Console.WriteLine($"RoundTrip time: {e.Reply.RoundtripTime}ms");
-            }
-            else
-            {
-                System.Console.WriteLine("It didn't work");
-                System.Console.WriteLine(e.Error.ToString());
-            }
+            var aTimer = new Timer(time);
+            aTimer.Elapsed += onElapsed;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            return aTimer;
         }
     }
 }
